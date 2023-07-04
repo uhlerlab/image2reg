c8b1831b4514f922ecbbb59ba5d2ea3992761a03
Daniel Paysan <52237795+dpaysan@users.noreply.github.com>
2023-07-04 09:40:26 +0200

    Update download_rohban_data.sh

--- scripts/data/download_rohban_data.sh
+++ scripts/data/download_rohban_data.sh
@@ -8,7 +8,7 @@ output_dir = "./data/resources/images/rohban/raw"
 if [ -d "$output_dir" ]; then
 	echo "$output_dir already exist. Exiting to prevent data loss..."
 else
-	mkdir "$output_dir"
+	mkdir -p "$output_dir"
 	chmod 777 -R "$output_dir"
 	cd "$output_dir" || exit
 	echo "Downloading ssh key..."
